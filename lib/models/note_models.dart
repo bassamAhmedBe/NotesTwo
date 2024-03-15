69d21e2fc3643005cb4c7078c8b084dd97148589
Your Name <you@example.com>
2024-03-15 18:53:52 +0200

    add Note-model

---
+++ lib/models/note_models.dart
@@ -0,0 +1,13 @@
+class NoteModels {
+  final String title;
+  final String subtitle;
+  final int date;
+  final String colors;
+
+  NoteModels({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.colors,
+  });
+}
