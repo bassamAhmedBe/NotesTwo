a3b05ef080e87f081fdf9a0d99208e33c4adb0b9
Your Name <you@example.com>
2024-03-15 19:09:13 +0200

    add hive pacakges

--- lib/models/note_models.dart
+++ lib/models/note_models.dart
@@ -1,8 +1,8 @@
 class NoteModels {
   final String title;
   final String subtitle;
-  final int date;
-  final String colors;
+  final String date;
+  final int colors;
 
   NoteModels({
     required this.title,
