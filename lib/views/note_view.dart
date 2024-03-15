db5a5ef1a8f0f1795ccc28fc311788db83dd7b09
Your Name <you@example.com>
2024-03-15 14:43:09 +0200

    create custom noteItem

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -6,8 +6,9 @@ class NoteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: NoteViewBody(),
+      
     );
   }
 }
--- lib/widgets/note_view_body.dart
+++ lib/widgets/note_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_two/constant.dart';
 import 'package:note_two/widgets/custom_appBar.dart';
+import 'package:note_two/widgets/custom_note_item.dart';
 
 class NoteViewBody extends StatelessWidget {
   const NoteViewBody({super.key});
@@ -7,13 +9,17 @@ class NoteViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 10),
+      padding: EdgeInsets.symmetric(horizontal: 10),
       child: Column(
         children: [
           SizedBox(
             height: 30,
           ),
           CustomAppBar(),
+          SizedBox(
+            height: 10,
+          ),
+          NoteItem(),
         ],
       ),
     );
