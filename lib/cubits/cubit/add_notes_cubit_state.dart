1a7f21b66ff01f40185b81548141ac18b9c137dc
Your Name <you@example.com>
2024-03-15 23:29:50 +0200

    add note cubit state

--- lib/cubits/cubit/add_notes_cubit_state.dart
+++ lib/cubits/cubit/add_notes_cubit_state.dart
@@ -3,3 +3,12 @@ part of 'add_notes_cubit_cubit.dart';
 sealed class AddNotesCubitState {}
 
 final class AddNotesCubitInitial extends AddNotesCubitState {}
+
+final class AddNotesCubitLoading extends AddNotesCubitState {}
+
+final class AddNotesCubitISucces extends AddNotesCubitState {}
+
+class AddNotesCubitFailure extends AddNotesCubitState {
+  final String errorMessage;
+  AddNotesCubitFailure(this.errorMessage);
+}
