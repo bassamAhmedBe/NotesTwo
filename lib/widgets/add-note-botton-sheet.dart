7a8db4de2f30900deae27cd04af0c343d2e8bbfa
Your Name <you@example.com>
2024-03-15 16:33:26 +0200

    create button and finish shet

--- lib/widgets/add-note-botton-sheet.dart
+++ lib/widgets/add-note-botton-sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_two/widgets/custom_button.dart';
 import 'package:note_two/widgets/custom_textfield.dart';
 
 class AddNoteBottonShet extends StatelessWidget {
@@ -7,23 +8,29 @@ class AddNoteBottonShet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.all(16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 24,
-          ),
-          CustomTextField(
-            hintText: 'Title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            maxLine: 5,
-            hintText: 'content',
-          ),
-        ],
+      padding: EdgeInsets.all(16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              maxLine: 5,
+              hintText: 'content',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomButton(),
+          ],
+        ),
       ),
     );
   }
