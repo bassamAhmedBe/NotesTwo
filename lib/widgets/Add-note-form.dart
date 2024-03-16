ec6cc3201f54830d94f61940e9ca7197c91ec0c3
Your Name <you@example.com>
2024-03-16 16:15:31 +0200

    AAA

--- lib/widgets/Add-note-form.dart
+++ lib/widgets/Add-note-form.dart
@@ -1,1 +1,64 @@
 
+
+import 'package:flutter/material.dart';
+import 'package:note_two/widgets/custom_button.dart';
+import 'package:note_two/widgets/custom_textfield.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formkey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 24,
+          ),
+          CustomTextField(
+            onsaved: (value) {
+              title = value;
+            },
+            hintText: 'Title',
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onsaved: (value) {
+              subtitle = value;
+            },
+            maxLine: 5,
+            hintText: 'content',
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomButton(
+            ontab: () {
+              if (formkey.currentState!.validate()) {
+                formkey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/add-note-botton-sheet.dart
+++ lib/widgets/add-note-botton-sheet.dart
@@ -1,75 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:note_two/widgets/custom_button.dart';
-import 'package:note_two/widgets/custom_textfield.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_two/cubits/cubit/add_notes_cubit_cubit.dart';
+import 'package:note_two/widgets/Add-note-form.dart';
 
 class AddNoteBottonShet extends StatelessWidget {
   const AddNoteBottonShet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(16),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
-      ),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formkey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title, subtitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formkey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 24,
-          ),
-          CustomTextField(
-            onsaved: (value) {
-              title = value;
-            },
-            hintText: 'Title',
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            onsaved: (value) {
-              subtitle = value;
-            },
-            maxLine: 5,
-            hintText: 'content',
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomButton(
-            ontab: () {
-              if (formkey.currentState!.validate()) {
-                formkey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-        ],
+        child: BlocConsumer<AddNotesCubitCubit, AddNotesCubitState>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            return AddNoteForm();
+          },
+        ),
       ),
     );
   }
